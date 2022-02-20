919508de70eb4fc0d634bdfc9156eab2d850d36b
Jack <jack.fernald@gmail.com>
2022-02-19 20:08:27 -0700

    Version 1 of desktop gui

---
+++ test.js
@@ -0,0 +1,8 @@
+const CHILD_TEMPALTE = '<div class="cards"><div class="cards-title"><h2>Solutions to problems that may or may not exist</h2></div><div class="cards-body"><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta ducimus in, delectus excepturi mollitia eveniet nisi rem, impedit harum tempore amet ut,dolorum sed ullam. Sequi accusamus ea aspernatur possimus.</p></div></div>'
+
+
+function testChildCard(number) {
+    for(let i = 0; i < number; i++){
+        $('.main-panel').append(CHILD_TEMPALTE)
+    }
+}
